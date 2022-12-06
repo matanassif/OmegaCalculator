7762452fc86f3339cfadd1dc383ddb8d75a779fa
Matan Assif <matanasif19@gmail.com>
2022-12-06 19:07:31 +0200

    Adding dictionaries of priority, operator-function, position

--- Configuration.py
+++ Configuration.py
@@ -1,1 +1,16 @@
-#Configuration File
+# Configuration File which contains all the global constants
+
+import Arithmetic_Functions as Arethmetics
+
+# A dictionary of every operator and its priority
+priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5, '~': 6, '!': 6, '#': 6}
+
+# A dictionary of every operator and its function
+arithmetic_function = {'+': Arethmetics.add, '-': Arethmetics.sub, '*': Arethmetics.mul, '/': Arethmetics.div,
+                       '^': Arethmetics.power, '%': Arethmetics.modulo, '$': Arethmetics.maximum,
+                       '&': Arethmetics.minimum, '@': Arethmetics.average, '~': Arethmetics.negative,
+                       '!': Arethmetics.factorial, '#': Arethmetics.add_digits}
+
+# A dictionary of every operator and its position
+position = {'+': "mid", '-': "mid", '*': "mid", '/': "mid", '^': "mid", '%': "mid", '$': "mid",
+            '&': "mid", '@': "mid", '~': "lef", '!': "right", '#': "right"}
