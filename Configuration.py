e502646a4f83dfd39598f300c422fac78f2659e7
Matan Assif <matanasif19@gmail.com>
2022-12-15 16:31:41 +0200

    adding list of operators for changes in converting to postfix

--- Configuration.py
+++ Configuration.py
@@ -1,19 +1,19 @@
 # Configuration File which contains all the global constants
 
-import Arithmetic_Functions as Arethmetics
+import Arithmetic_Functions as Arithmetics
 
 # A dictionary of every operator and its priority
 priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5, '~': 6, '!': 6, '#': 6, '(': 7}
 
 # A dictionary of every operator and its function
-arithmetic_function = {'+': Arethmetics.add, '-': Arethmetics.sub, '*': Arethmetics.mul, '/': Arethmetics.div,
-                       '^': Arethmetics.power, '%': Arethmetics.modulo, '$': Arethmetics.maximum,
-                       '&': Arethmetics.minimum, '@': Arethmetics.average, '~': Arethmetics.negative,
-                       '!': Arethmetics.factorial, '#': Arethmetics.add_digits}
+arithmetic_function = {'+': Arithmetics.add, '-': Arithmetics.sub, '*': Arithmetics.mul, '/': Arithmetics.div,
+                       '^': Arithmetics.power, '%': Arithmetics.modulo, '$': Arithmetics.maximum,
+                       '&': Arithmetics.minimum, '@': Arithmetics.average, '~': Arithmetics.negative,
+                       '!': Arithmetics.factorial, '#': Arithmetics.add_digits}
 
 # A dictionary of every operator and its position
 position = {'+': "mid", '-': "mid", '*': "mid", '/': "mid", '^': "mid", '%': "mid", '$': "mid",
-            '&': "mid", '@': "mid", '~': "lef", '!': "right", '#': "right"}
+            '&': "mid", '@': "mid", '~': "left", '!': "right", '#': "right"}
 
 # A list of all white spaces
 white_spaces = [" ", "\t", "\n"]
@@ -21,3 +21,6 @@ white_spaces = [" ", "\t", "\n"]
 # A list of all legal elements in the given mathematical expression
 legal_elements = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' ', '+', '-', '*', '/', '^', '%', '$', '&',
                   '@', '~', '!', '#', '(', ')']
+
+# A list of all legal operators
+operators = ['+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!', '#', '(', ')']
